2712d6416d049672bb7e98eb6b4fc335b7c2f597
shamikam <shamika.malwattha@sysco.com>
2018-08-01 21:54:51 +0530

    initial commit to start web_ui_automation project

---
+++ src/main/java/com/sysco/web_ui_automation/data/LoginData.java
@@ -0,0 +1,11 @@
+package com.sysco.web_ui_automation.data;
+
+public class LoginData {
+
+    public static String ref;
+    public static String box;
+    public static String userName;
+    public static String password;
+    public static String iPAddress;
+
+}
